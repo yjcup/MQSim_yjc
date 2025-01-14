89c31a6894c1c64e933e86eb8ca433044914b340
yjc <sett6@qq.com>
2025-01-14 15:56:29 +0800

    start writing hybridSSD

--- src/utils/XMLWriter.cpp
+++ src/utils/XMLWriter.cpp
@@ -4,25 +4,28 @@
 
 namespace Utils
 {
-	bool XmlWriter::exists(const std::string fileName) {
+	bool XmlWriter::exists(const std::string fileName)
+	{
 		std::fstream checkFile(fileName);
 		return checkFile.is_open();
 	}
-	
 
-	bool CreateDirectoryIfNeeded(const std::string& dirPath) {
+	bool CreateDirectoryIfNeeded(const std::string &dirPath)
+	{
 		// 创建目录，如果目录已存在，则返回 true
-		if (mkdir(dirPath.c_str(), 0777) == 0 || errno == EEXIST) {
+		if (mkdir(dirPath.c_str(), 0777) == 0 || errno == EEXIST)
+		{
 			return true;
 		}
 		return false;
 	}
 
-
-			bool XmlWriter::Open(const std::string strFile) {
+	bool XmlWriter::Open(const std::string strFile)
+	{
 
 		outFile.open(strFile);
-		if (outFile.is_open()) {
+		if (outFile.is_open())
+		{
 			outFile << "<?xml version=\"1.0\" encoding=\"us-ascii\"?>\n";
 			indent = 0;
 			openTags = 0;
@@ -32,29 +35,29 @@ namespace Utils
 		}
 
 		return false;
-
 	}
 
+	bool XmlWriter::Open(const std::string strFile, const std::string timeStr)
+	{
+		// 获取当前时间作为文件夹名
+		// std::string timeStr = GetCurrentTimeAsString();
 
-	bool XmlWriter::Open(const std::string strFile,const std::string timeStr) {
-      // 获取当前时间作为文件夹名
-    // std::string timeStr = GetCurrentTimeAsString();
+		// 基于当前时间构建文件夹路径
+		std::string dirPath = "./result/" + timeStr; // 使用时间戳创建文件夹路径，路径格式是 ./2024-12-15_14-30-45
 
-    // 基于当前时间构建文件夹路径
-    std::string dirPath = "./result/" + timeStr;  // 使用时间戳创建文件夹路径，路径格式是 ./2024-12-15_14-30-45
+		// 创建目录（如果它不存在）
+		if (!CreateDirectoryIfNeeded(dirPath))
+		{
+			std::cerr << "Error creating directory for time: " << timeStr << std::endl;
+			return false;
+		}
 
-    // 创建目录（如果它不存在）
-    if (!CreateDirectoryIfNeeded(dirPath)) {
-        std::cerr << "Error creating directory for time: " << timeStr << std::endl;
-        return false;
-    }
-
-    // 构建完整的文件路径：文件夹路径 + 文件名
-    std::string fullFilePath = dirPath + "/" + strFile;  // 假设 strFile 是文件名
-
+		// 构建完整的文件路径：文件夹路径 + 文件名
+		std::string fullFilePath = dirPath + "/" + strFile; // 假设 strFile 是文件名
 
 		outFile.open(fullFilePath);
-		if (outFile.is_open()) {
+		if (outFile.is_open())
+		{
 			outFile << "<?xml version=\"1.0\" encoding=\"us-ascii\"?>\n";
 			indent = 0;
 			openTags = 0;
@@ -65,17 +68,21 @@ namespace Utils
 
 		return false;
 	}
-	
+
 	void XmlWriter::Close()
 	{
-		if (outFile.is_open()) {
+		if (outFile.is_open())
+		{
 			outFile.close();
 		}
 	}
-	
-	void XmlWriter::Write_open_tag(const std::string openTag) {
-		if (outFile.is_open()) {
-			for (int i = 0; i < indent; i++) {
+
+	void XmlWriter::Write_open_tag(const std::string openTag)
+	{
+		if (outFile.is_open())
+		{
+			for (int i = 0; i < indent; i++)
+			{
 				outFile << "\t";
 			}
 			tempOpenTag.resize(openTags + 1);
@@ -83,87 +90,115 @@ namespace Utils
 			tempOpenTag[openTags] = openTag;
 			indent += 1;
 			openTags += 1;
-		} else {
+		}
+		else
+		{
 			PRINT_ERROR("The XML output file is closed. Unable to write to file");
 		}
 	}
-	
+
 	void XmlWriter::Write_attribute_string(const std::string attribute_name, const std::string attribute_value)
 	{
-		if (outFile.is_open()) {
-			for (int i = 0; i < indent + 1; i++) {
+		if (outFile.is_open())
+		{
+			for (int i = 0; i < indent + 1; i++)
+			{
 				outFile << "\t";
 			}
 
 			outFile << " <" << attribute_name + ">" + attribute_value + "</" << attribute_name + ">\n";
-		} else {
+		}
+		else
+		{
 			PRINT_ERROR("The XML output file is closed. Unable to write to file");
 		}
 	}
-	
-	void XmlWriter::Write_close_tag() {
-		if (outFile.is_open()) {
+
+	void XmlWriter::Write_close_tag()
+	{
+		if (outFile.is_open())
+		{
 			indent -= 1;
-			for (int i = 0; i < indent; i++) {
+			for (int i = 0; i < indent; i++)
+			{
 				outFile << "\t";
 			}
 			outFile << "</" << tempOpenTag[openTags - 1] << ">\n";
 			tempOpenTag.resize(openTags - 1);
 			openTags -= 1;
-		} else {
+		}
+		else
+		{
 			PRINT_ERROR("The XML output file is closed. Unable to write to file");
 		}
 	}
-	
-	void XmlWriter::Write_start_element_tag(const std::string elementTag) {
-		if (outFile.is_open()) {
-			for (int i = 0; i < indent; i++) {
+
+	void XmlWriter::Write_start_element_tag(const std::string elementTag)
+	{
+		if (outFile.is_open())
+		{
+			for (int i = 0; i < indent; i++)
+			{
 				outFile << "\t";
 			}
 			tempElementTag.resize(openElements + 1);
 			tempElementTag[openElements] = elementTag;
 			openElements += 1;
 			outFile << "<" << elementTag;
-		} else {
+		}
+		else
+		{
 			PRINT_ERROR("The XML output file is closed. Unable to write to file");
 		}
 	}
 
 	void XmlWriter::Write_end_element_tag()
 	{
-		if (outFile.is_open()) {
+		if (outFile.is_open())
+		{
 			outFile << "/>\n";
 			tempElementTag.resize(openElements - 1);
 			openElements -= 1;
-		} else {
+		}
+		else
+		{
 			PRINT_ERROR("The XML output file is closed. Unable to write to file");
 		}
 	}
 
 	void XmlWriter::Write_attribute(const std::string outAttribute)
 	{
-		if (outFile.is_open()) {
+		if (outFile.is_open())
+		{
 			outFile << " " << outAttribute;
-		} else {
+		}
+		else
+		{
 			PRINT_ERROR("The XML output file is closed. Unable to write to file");
 		}
 	}
 
 	void XmlWriter::Write_attribute_string_inline(const std::string attribute_name, const std::string attribute_value)
 	{
-		if (outFile.is_open()) {
+		if (outFile.is_open())
+		{
 			outFile << " ";
 			outFile << attribute_name + "=\"" + attribute_value + "\"";
-		} else {
+		}
+		else
+		{
 			PRINT_ERROR("The XML output file is closed. Unable to write to file");
 		}
 	}
 
 	void XmlWriter::Write_string(const std::string outString)
 	{
-		if (outFile.is_open()) {
+		if (outFile.is_open())
+		{
 			outFile << ">" << outString;
-		} else {
+		}
+		else
+		{
 			PRINT_ERROR("The XML output file is closed. Unable to write to file");
 		}
 	}
